27cdbf11cb83d56c17ccecac60e170c5bbc1df54
MichaelDimmitt <michaeldimmitt@MichaelDimmitts-MacBook-Pro.local>
2018-10-16 23:14:00 -0400

    implement the reusable structure for further checkpoints

--- check.sh
+++ check.sh
@@ -1,1 +1,12 @@
-launchctl list | awk '{print $3}' > temp; diff temp mac_sierra_vanilla.txt; rm -rf temp;
+check_agent mac_sierra_vanilla
+
+check_agent(){
+  if ! [ -f $1.txt ]; then
+    launchctl list | awk '{print $3}' > $1.txt  
+  fi
+  
+  launchctl list | awk '{print $3}' > temp; 
+  diff $1.txt temp # > overflow;
+  rm -rf temp;
+}
+

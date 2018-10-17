6259e31c2507db3f6661929012118873e31a197e
MichaelDimmitt <michaeldimmitt@MichaelDimmitts-MacBook-Pro.local>
2018-10-17 11:31:31 -0400

    for each directory, continue piping, lastly, check_if_empty

--- check.sh
+++ check.sh
@@ -1,12 +1,33 @@
 check_agent mac_sierra_vanilla
 
 check_agent(){
-  if ! [ -f $1.txt ]; then
-    launchctl list | awk '{print $3}' > $1.txt  
-  fi
-  
+  if ! [ -f $1.txt ]; then launchctl list | awk '{print $3}' > $1.txt; fi
+
   launchctl list | awk '{print $3}' > temp; 
-  diff $1.txt temp # > overflow;
+
+  var=$(diff $1.txt temp) # > overflow
+
+  check_empty "$var"
   rm -rf temp;
 }
+
+check_empty(){
+  # check if empty
+  if test -z "$1"; then
+    echo "\$var is empty"
+  else
+    echo -e "\$var is NOT empty, here is var: \n$1";
+  fi
+}
+
+# if option passed for insert new checkpoint..
+
+# note for future implementation.
+# for d in each * in dir
+# check_agent $d
+
+# otherwise ls-count
+# num=$(ls -1 | wc -l | awk '{ print $1}')
+
+# check_agent checkpoint$num
 
